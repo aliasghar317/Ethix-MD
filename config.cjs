9beb1ad69b33d3526bf9b38b74ac497eb8e99cd8
aliasghar317 <135347876+aliasghar317@users.noreply.github.com>
2024-06-16 07:01:15 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -18,8 +18,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || 'public',
-  OWNER_NAME: process.env.OWNER_NAME || "✪⏤͟͞★⃝ꪶ‎𝞢𝙏𝞖𝞘𝞦-𝞛𝘿𖥘✪͜͡➺",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "919142294671",
+  OWNER_NAME: process.env.OWNER_NAME || "✪⏤͟͞★⃝ꪶ‎𝛥𝐿𝛪-𝛥𝑆𝐺𝛨𝛥𝑅𖥘✪͜͡➺",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "923168687317",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
 
